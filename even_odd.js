a837ec54fae74a1893278fd691aeda8697080b23
ahsan9542 <117145100+ahsan9542@users.noreply.github.com>
2022-11-01 12:36:58 +0530

    Update even_odd.js

--- even_odd.js
+++ even_odd.js
@@ -2,9 +2,9 @@
 2 
 3 let num=10;
 4
-5 if (num%3==0) {
-6     console.log("Even");
+5 if (num%2==0) {
+6     console.log("Even Number");
 7 } else {
-8 console.log("odd");
+8     console.log("Odd Number");
 9 }
    
